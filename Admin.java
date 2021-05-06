b11d76622bc771c38733391cc7003d72d584cfc8
qennzo <75254422+qennzo@users.noreply.github.com>
2021-05-06 12:17:16 +0200

    Create Admin.java
    Clase Admin de su respectiva tabla

---
+++ Admin.java
@@ -0,0 +1,68 @@
+/* DATALIS ADMIN TABLE */
+
+public class Admin {
+    
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String tlf;
+    private String password;
+
+    public Admin(String dni, String nombre, String apellidos, String email, String tlf, String password) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.tlf = tlf;
+        this.password = password;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
