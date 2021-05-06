f727892ba343baad38b3d6e9835687ba874eb114
qennzo <75254422+qennzo@users.noreply.github.com>
2021-05-06 12:20:29 +0200

    Create Offers.java
    Clase Offers de su respectiva tabla

---
+++ Offers.java
@@ -0,0 +1,10 @@
+/* DATALIS OFFERS TABLE */
+
+public class Offers {
+    
+    private int id;
+    private String dni_admin;
+    private String descripcion;
+    private String especialidad;
+    
+}
